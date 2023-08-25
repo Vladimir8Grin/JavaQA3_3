bdc5823569ba99b1fd86bd204c6c9da4c0d55e28
“Vladimir_Grin” <mortywallet@gmail.com>
2023-08-25 23:19:42 +0300

    gitqa3_3

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,5 @@
 public class CreditPaymentService {
     public static double calculate(int credit, double mMonth, int namMonth) {
-        //ann = c * ((r * Math.pow((1 + r), 12 * y)) / (Math.pow((1 + r), 12 * y) - 1));
         return (int) credit * ((mMonth * Math.pow((1 + mMonth), namMonth)) / (Math.pow((1 + mMonth), namMonth) - 1));
     }
 }
