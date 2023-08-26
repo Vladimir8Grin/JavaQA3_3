040bc6c79f2447b79e421d510bff86937a2965dc
“Vladimir_Grin” <mortywallet@gmail.com>
2023-08-26 12:20:45 +0300

    gitqa3_3

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         int credit = 1_000_000;
-        int NamMonth = 36;
+        int namMonth = 36;
         double payment;
         double mGod = 9.99;
         double mMonth = (mGod / 100) / 12;
-        payment = CreditPaymentService.calculate(credit, mMonth, NamMonth);
+        payment = CreditPaymentService.calculate(credit, mMonth, namMonth);
         System.out.println("Месячная оплата по кредиту составляет " + Math.round(payment) + " рублей");
     }
 }
